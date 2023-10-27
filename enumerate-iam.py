2917ce58800152d05ab4abc2b732f3fcf30ae5f9
August Detlefsen <adetlefsen@flexport.com>
2023-10-26 18:18:31 -0700

    Accept access-key and secret-key as input instead of command line
    To avoid leaving those values in shell history

--- enumerate-iam.py
+++ enumerate-iam.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/opt/homebrew/bin/python3
 import argparse
 
 from enumerate_iam.main import enumerate_iam
@@ -7,17 +7,23 @@ from enumerate_iam.main import enumerate_iam
 def main():
     parser = argparse.ArgumentParser(description='Enumerate IAM permissions')
 
-    parser.add_argument('--access-key', help='AWS access key', required=True)
-    parser.add_argument('--secret-key', help='AWS secret key', required=True)
+    parser.add_argument('--access-key', help='AWS access key')
+    parser.add_argument('--secret-key', help='AWS secret key')
     parser.add_argument('--session-token', help='STS session token')
     parser.add_argument('--region', help='AWS region to send API requests to', default='us-east-1')
 
     args = parser.parse_args()
 
+    if args.access_key is None:
+        args.access_key = input("Enter AWS access key: ")
+
+    if args.secret_key is None:
+        args.secret_key = input("Enter AWS secret key: ")
+
     enumerate_iam(args.access_key,
-                  args.secret_key,
-                  args.session_token,
-                  args.region)
+                      args.secret_key,
+                      args.session_token,
+                      args.region)
 
 
 if __name__ == '__main__':
